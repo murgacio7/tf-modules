865b871dae4efcb31bcfbb096a97672ff1deebee
“Murat <mnesiren14@gmail.com>
2024-03-26 20:03:48 -0500

    changed the format of the file

--- gke/gke.tf
+++ gke/gke.tf
@@ -1,17 +1,15 @@
 resource "google_service_account" "default" {
-  account_id   = var.sa_account_id   #"gke-cluster"
-  display_name = var.sa_display_name #"service account for gke cluster"
+  account_id   = var.sa_account_id   # "gke-cluster"
+  display_name = var.sa_display_name # "service account for gke cluster"
 }
 
 resource "google_container_cluster" "terra4m" {
-  name                = var.cluster_name        #"my-gke-cluster2"
-  location            = var.cluster_location    #"us-central1-b"
-  initial_node_count  = var.node_count          #3
-  
+  name                = var.cluster_name        # "my-gke-cluster2"
+  location            = var.cluster_location    # "us-central1-b"
+  initial_node_count  = var.node_count          # 3
 
   node_config {
-    disk_size_gb    = var.disk_size_gb                     #60
+    disk_size_gb    = var.disk_size_gb                     # 60
     service_account = "63132520703-compute@developer.gserviceaccount.com"
+  }
 }
-
-
