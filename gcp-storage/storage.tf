ee8b87b1b9eae961ce8a6bc70a0119927b7fff52
“Murat <mnesiren14@gmail.com>
2024-03-27 12:53:10 -0500

    edited storage file

--- gcp-storage/storage.tf
+++ gcp-storage/storage.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "static" {
  name          = var.bucket_name  
  location      = var.bucket_location
- storage_class = var.bucket_storage-class 
+ storage_class = var.bucket_storage_class 
 
  uniform_bucket_level_access = var.level_access 
 }
